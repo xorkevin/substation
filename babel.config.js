24ea2a1d0e683c1ae4f4deb45ce44c26ffbca347
Kevin Wang <kevin@xorkevin.com>
2019-10-08 17:54:31 -0700

    Add webpack build

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+const createConfig = (api) => {
+  api.cache.never();
+  return {
+    presets: [
+      '@babel/preset-react',
+      [
+        '@babel/env',
+        {
+          modules: false,
+          useBuiltIns: false,
+          targets: {
+            browsers: [
+              'last 2 Chrome versions',
+              'last 2 Firefox versions',
+              'last 1 Safari versions',
+              'last 1 ChromeAndroid versions',
+              'last 1 Edge versions',
+            ],
+          },
+        },
+      ],
+    ],
+    plugins: ['@babel/syntax-dynamic-import'],
+  };
+};
+
+module.exports = createConfig;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'core-js/stable';
 import React, {
   useState,
   useEffect,
